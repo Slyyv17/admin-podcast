320d85bdc1eba29003be24553bd80409cc78ae9d
VictorDev <ememevictor08@gmail.com>
2025-06-18 11:53:52 +0100

    settings added

---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,7 @@
+export default function SettingsPage() {
+    return (
+        <main className="min-h-screen w-full flex items-center justify-center">
+            <h1 className="text-2xl font-bold">Settings Page</h1>
+        </main>
+    );
+}
